b10a8a979aab15b9e1140853f5ad68b3bbc5996d
Lucas Ferreira Torres de Albuquerque <lfta@cesar.school>
2024-11-27 15:20:11 -0300

    popup

---
+++ LibreflixTV/LibreflixApp/static/js/popup.js
@@ -0,0 +1,29 @@
+function popup(id) {
+    const popup = document.getElementById(id);
+    const backdrop = document.querySelector('.backdrop');
+    
+    if(popup.style.display === 'block') {
+        closePopup();
+    } else {
+        openPopup();
+    }
+
+    function openPopup() {
+        popup.style.display = 'block';
+        backdrop.style.display = 'block';
+        
+        popup.style.animation = "fadeIn 0.5s forwards";
+        backdrop.style.animation = "fadeIn 0.5s forwards";
+    }
+
+    function closePopup() {
+        popup.style.animation = "fadeOut 0.5s forwards";
+        backdrop.style.animation = "fadeOut 0.5s forwards";
+
+        setTimeout(function() {
+            popup.style.display = 'none';
+            backdrop.style.display = 'none';
+        }, 500);
+
+    }
+}

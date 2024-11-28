1d0b3e9ee9b36e43494c2c1a1f45dd42b03d8cc3
drakonbr <161368150+ChrisDrakon@users.noreply.github.com>
2024-11-28 18:14:27 -0300

    realocado pagina login direto

--- LibreflixTV/projeto/urls.py
+++ LibreflixTV/projeto/urls.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import RedirectView
 
-from LibreflixApp.views import HomeView, PageView, FavoritosView, ObraView, CatalogoView, GeneroView, SairView, PesquisaView, LoginView, RegistrationView
+from LibreflixApp.views import (
+    HomeView,
+    PageView,
+    FavoritosView,
+    ObraView,
+    CatalogoView,
+    GeneroView,
+    SairView,
+    PesquisaView,
+    LoginView,
+    RegistrationView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', HomeView.as_view(), name='home'),
-    path('', PageView.as_view(), name='page'),
+    path('', RedirectView.as_view(url='/login/'), name='redirect_to_login'),
     path('favoritos/', FavoritosView.as_view(), name='favoritos'),
     path('obra/<int:id>/', ObraView.as_view(), name='obra_info'),
     path('generos/', CatalogoView.as_view(), name='generos'),
@@ -14,6 +26,6 @@ urlpatterns = [
     path('sair/', SairView.as_view(), name='sair'),
     path('pesquisa/<str:titulo>/', PesquisaView.as_view(), name='pesquisa'),
     path('pesquisa/', PesquisaView.as_view(), name='pesquisa_vazia'),
-    path('login/', LoginView.as_view(), name='login'),  
-    path('register/', RegistrationView.as_view(), name='register'),  
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegistrationView.as_view(), name='register'),
 ]
